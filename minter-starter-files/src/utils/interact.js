e4e1c142ed61d46b7a4638650a8cb3baccf44982
jeenusjunanio <jeenussvd@gmail.com>
2022-02-09 10:51:07 +0400

    commented env file

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -1,6 +1,6 @@
 // import {pinJSONToIPFS} from './pinata.js';
 import axios from 'axios';
-require('dotenv').config();
+// require('dotenv').config();
 // const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 // const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 // const web3 = createAlchemyWeb3(alchemyKey);
